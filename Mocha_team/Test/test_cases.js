754138bf81756a94ed986c002ac7c8dd4855eb5a
Shwetarishal <92520628+Shwetarishal@users.noreply.github.com>
2021-10-19 16:15:04 +0530

    Add files via upload

---
+++ Mocha_team/Test/test_cases.js
@@ -0,0 +1,14 @@
+var chai = require('chai')
+var expect = require('chai').expect
+
+var logic = require('../default_test')
+
+
+describe('CASE',function(){
+    it('Case should run' , function(){
+        expect(logic()).to.eql('hello there');
+    })
+     it('2nd Case is running' , function(){
+        expect(logic()).to.eql('hello there');
+    })
+})
